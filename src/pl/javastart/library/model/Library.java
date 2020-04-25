0642ab2e76de8aa134652c67802aff2f4b699823
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-04-25 19:48:08 +0200

    Add static to code

--- src/pl/javastart/library/model/Library.java
+++ src/pl/javastart/library/model/Library.java
@@ -1,27 +1,27 @@
 package pl.javastart.library.model;
 
 public class Library {
-    private final int maxBooks = 1000;
-    private Book [] books = new Book[maxBooks];
+
+    private static final int MAX_BOOKS = 1000;
+    private Book[] books = new Book[MAX_BOOKS];
     private int booksNumber;
 
-    public void addBook(Book book){
-        if(booksNumber<maxBooks){
+    public void addBook(Book book) {
+        if (booksNumber < MAX_BOOKS) {
             books[booksNumber] = book;
             booksNumber++;
-        }else{
-            System.out.println("Maksymalna liczba książek została osiągnięta");
+        } else {
+            System.out.println("Maxymalna liczba książek została osiągnięta");
         }
+
     }
 
-    public void printBooks(){
-        if(booksNumber==0){
-            System.out.println("Brak książek w bibliotece.");
+    public void printBooks() {
+        if (booksNumber == 0) {
+            System.out.println("Brak książek w bibliotece");
         }
-        for(int i = 0 ;i<booksNumber;i++){
+        for (int i = 0; i < booksNumber; i++) {
             books[i].printInfo();
         }
     }
-
-
 }
