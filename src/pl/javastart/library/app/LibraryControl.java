84a3da9047abc07a3b27a78c508374fc81b8b84e
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-06-17 22:36:06 +0200

    Add Comparator

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -12,7 +12,9 @@ import pl.javastart.library.model.Book;
 import pl.javastart.library.model.Library;
 import pl.javastart.library.model.Magazine;
 import pl.javastart.library.model.Publication;
+import pl.javastart.library.model.comparator.AlphabeticalTitleComparator;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
 
 class LibraryControl {
@@ -103,10 +105,7 @@ class LibraryControl {
         }
     }
 
-    private void printBooks() {
-        Publication[] publications = library.getPublications();
-        printer.printBooks(publications);
-    }
+
 
     private void addMagazine() {
         try {
@@ -119,10 +118,7 @@ class LibraryControl {
         }
     }
 
-    private void printMagazines() {
-        Publication[] publications = library.getPublications();
-        printer.printMagazines(publications);
-    }
+
 
     private void deleteMagazine() {
         try {
@@ -188,5 +184,21 @@ class LibraryControl {
                 throw new NoSuchOptionException("Brak opcji o id " + option);
             }
         }
+    }
+
+    private void printBooks() {
+        Publication[] publications = getSortedPublications();
+        printer.printBooks(publications);
+    }
+
+    private void printMagazines() {
+        Publication[] publications = getSortedPublications();
+        printer.printMagazines(publications);
+    }
+
+    private Publication[] getSortedPublications() {
+        Publication[] publications = library.getPublications();
+        Arrays.sort(publications, new AlphabeticalTitleComparator());
+        return publications;
     }
 }
