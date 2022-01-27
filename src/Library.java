583e973b2a6ac014e2e60eae9587f298e72fa344
mateu <mateusz.wygoda>
2022-01-27 23:18:42 +0100

    Initial commit

---
+++ src/Library.java
@@ -0,0 +1,14 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.1";
+
+        String title = "W pustyni i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releaseDate = 1990;
+        int pages = 296;
+        String publisher = "Greg";
+        String isbn = "923412532";
+
+
+    }
+}
