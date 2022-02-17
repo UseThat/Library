57a3b21ba5042e5913f1004262e95862aa40ed4b
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2022-02-17 23:20:20 +0100

    Add Inheritance

--- src/pl/javastart/library/app/LibraryApp.java
+++ src/pl/javastart/library/app/LibraryApp.java
@@ -1,9 +1,9 @@
 package pl.javastart.library.app;
 
 public class LibraryApp {
-    private static final String APP_NAME = "Biblioteka v1.0";
-    public static void main(String[] args) {
+    private static final String APP_NAME = "Biblioteka v1.1";
 
+    public static void main(String[] args) {
         System.out.println(APP_NAME);
         LibraryControl libControl = new LibraryControl();
         libControl.controlLoop();
